048b057b00e9469883a279c499333f9a09f506e3
Xuelian Li <lixuelian8807@gmail.com>
2018-10-19 23:32:21 -0400

    made congratulation page, added animation effect

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-/*
- * Create a list that holds all of your cards
- */
+// global variable
+let openCards = [];
+let finalMove = 0;
+let starCount = 0;
+
+//Create a list that holds all of your cards
  let cards = ['fa-diamond','fa-diamond',
               'fa-paper-plane-o','fa-paper-plane-o',
               'fa-anchor','fa-anchor',
@@ -10,12 +13,7 @@
               'fa-bicycle','fa-bicycle',
               'fa-bomb','fa-bomb'];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// generate card using the card list
  function generateCards(cards){
    let deck = document.querySelector('.deck');
    cards.forEach(function(card){
@@ -42,28 +40,18 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-let openCards = [];
-let finalMove = 0;
-
+//initilization in begin, restart and play agian situation
 function initilization(){
   var deck = document.querySelector('.deck');
-  deck.innerHTML='';
+  while(deck.firstChild){
+    deck.removeChild(deck.firstChild);
+  }
+  document.querySelector('.congratBox').style.display='none';
   document.querySelector('.moves').innerText='0';
   document.querySelector('.stars').innerHTML=`<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
   openCards = [];
   finalMove = 0;
+  starCount = 0;
   generateCards(shuffle(cards));
 }
 
@@ -79,7 +67,8 @@ wholeDeck.addEventListener('click',function(event){
   if(event.target.classList.contains('card')){
     var cardClicked = event.target;
     let moveEvent = document.querySelector('.moves'); // read the movement.
-      //flip the card only it hasn't been showned and open situation.
+
+    //flip the card only it hasn't been showned and open situation.
     if(!cardClicked.classList.contains('open') && !cardClicked.classList.contains('show')){
         cardClicked.classList.add('open','show');
         openCards.push(cardClicked);
@@ -91,21 +80,31 @@ wholeDeck.addEventListener('click',function(event){
       let moveNo = moveEvent.innerText;
       starScore(moveNo);
 
+      // check match or not
       if(openCards[0].id === openCards[1].id){
         matchSituation(openCards);
+      }else{
+        openCards.forEach(function(element){
+        element.classList.add('wrong');
+        element.classList.add('wrong');
+        });
       }
 
+      //flip wrong card back
       setTimeout(function(){
         openCards.forEach(function(element){
           element.classList.remove('open','show');
+          if(element.classList.contains('wrong')){
+            element.classList.remove('wrong');
+          }
         });
         openCards=[];
-      },400);
+      },1000);
     }
   }
 });
 
-
+// handle match situation
 function matchSituation(openCards){
 
   openCards[0].classList.add('match');
@@ -114,30 +113,65 @@ function matchSituation(openCards){
   let moveNo = document.querySelector('.moves').innerText;
 
   // find all math cards.
-  console.log(`the matches are ${matches.length}`);
   if(matches.length === 16){
-    console.log(`the total movement is ${moveNo}`);
-     // display a new page.
-
+    finalMove = moveNo;
+    starCount = document.querySelectorAll('.fa-star').length;
+    congratulations();
   }
 }
 
+//show corresponding star rating
 function starScore(moveNo){
   let stars = document.querySelector('.stars');
   var twoStarsTemplate = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star-o"></i></li>`;
   var oneStarTemplate = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star-o"></i></li><li><i class="fa fa-star-o"></i></li>`;
   var noStarTemplate = `<li><i class="fa fa-star-o"></i></li><li><i class="fa fa-star-o"></i></li><li><i class="fa fa-star-o"></i></li>`;
 
-  if(moveNo <= 15 && moveNo > 10){
+  if(moveNo <= 20 && moveNo > 10){
     stars.innerHTML = '';
     stars.innerHTML = twoStarsTemplate;
-  }else if(moveNo <= 20 && moveNo > 15){
+  }else if(moveNo <= 30 && moveNo > 20){
     stars.innerHTML = '';
     stars.innerHTML = oneStarTemplate;
-  }else if(moveNo > 20){
+  }else if(moveNo > 30){
     stars.innerHTML = '';
     stars.innerHTML = noStarTemplate;
   }
 }
 
+//after find all matches
+function congratulations(){
+  document.querySelector('.container').style.display='none';// hide everything have before.
+  var newDiv = document.querySelector('.congratBox');
+
+  newDiv.style.display='inline';
+  newDiv.innerHTML='';
+  var template = `<section class='imageSection'>
+        <img class='checkbox rubberBand' src="img/checkbox-icon.png" alt="checkbox"/>
+        </section>
+        <section class='textSection'>
+        <p class='congrat'> Congratulations! You Won! </p>
+        <p class='rating'> With ${finalMove} Moves and ${starCount} Stars.</p>
+        <p class='cheerWords rubberBand'> Woooooo! </p>
+        </section>`;
+  newDiv.innerHTML = template;
+
+  var btn = document.createElement('button');
+  var t= document.createTextNode('Play Again!');
+  btn.className = 'playAgain';
+  btn.append(t);
+  newDiv.appendChild(btn);
+}
+
+//click play again button ,everything call initilization function
+var newDiv = document.querySelector('.congratBox')
+newDiv.addEventListener('click', function(event){
+  if(event.target.className ==='playAgain'){
+    console.log('clicked');
+    document.querySelector('.container').style.display='flex';
+    initilization();
+  }
+});
+
 initilization();
+//congratulations();
